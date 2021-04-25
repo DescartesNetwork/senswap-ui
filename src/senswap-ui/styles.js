5cd521a4f14d45cd6d458dd3eb8e350dc071f4a2
tuphan <tuphan@descartes.network>
2021-04-25 22:34:48 +0700

    release package

--- src/senswap-ui/styles.js
+++ src/senswap-ui/styles.js
@@ -1,3 +1,22 @@
-import { withStyles, useTheme, makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MuiThemeProvider, withStyles, useTheme, makeStyles } from '@material-ui/core/styles';
 
-export { withStyles, useTheme, makeStyles }
+import theme from 'senswap-ui/theme';
+
+function ThemeProvider(props) {
+  const { variant, ...others } = props;
+  if (variant === 'light') require('senswap-ui/light.css');
+  else require('senswap-ui/dark.css');
+  return <MuiThemeProvider theme={theme} {...others} />
+}
+
+ThemeProvider.defaultProps = {
+  variant: "dark",
+}
+
+ThemeProvider.propTypes = {
+  variant: PropTypes.string,
+}
+
+export { ThemeProvider, withStyles, useTheme, makeStyles }
