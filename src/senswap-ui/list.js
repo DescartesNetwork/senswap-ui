aac14ac5dc1f9e13422ccf1e86070cf9966d89ed
tphanson <tphanson@kambria.io>
2021-04-27 11:26:31 +0700

    refactor

--- src/senswap-ui/list.js
+++ src/senswap-ui/list.js
@@ -10,30 +10,30 @@ import MuiListSubheader from '@material-ui/core/ListSubheader';
 
 const List = forwardRef((props, ref) => {
   return <MuiList {...props} ref={ref} />
-})
+});
 
 export default List;
 
 export const ListItem = forwardRef((props, ref) => {
   return <MuiListItem {...props} ref={ref} />
-})
+});
 
 export const ListItemAvatar = forwardRef((props, ref) => {
   return <MuiListItemAvatar {...props} ref={ref} />
-})
+});
 
 export const ListItemIcon = forwardRef((props, ref) => {
   return <MuiListItemIcon {...props} ref={ref} />
-})
+});
 
 export const ListItemSecondaryAction = forwardRef((props, ref) => {
   return <MuiListItemSecondaryAction {...props} ref={ref} />
-})
+});
 
 export const ListItemText = forwardRef((props, ref) => {
   return <MuiListItemText {...props} ref={ref} />
-})
+});
 
 export const ListSubheader = forwardRef((props, ref) => {
   return <MuiListSubheader {...props} ref={ref} />
-})
+});
