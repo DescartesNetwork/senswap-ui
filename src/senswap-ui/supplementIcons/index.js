922c01230141fc466b526da47b85746fd1d1f788
thangDescartesNetwork <thangnguyen@descartes.network>
2021-05-07 14:55:43 +0700

    Add supplement icons

---
+++ src/senswap-ui/supplementIcons/index.js
@@ -0,0 +1,6 @@
+import HomeIcon from "./HomeIcon";
+import SwapIcon from "./SwapIcon";
+import PoolsIcon from "./PoolsIcon";
+import WalletIcon from "./WalletIcon";
+
+export { HomeIcon, SwapIcon, PoolsIcon, WalletIcon }
