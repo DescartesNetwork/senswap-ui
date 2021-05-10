7d57729710029e59bb01d7bb1c9df7ccc83c407b
tuphan <tuphan@descartes.network>
2021-05-10 11:44:30 +0700

    fix typography

--- src/senswap-ui/typography.js
+++ src/senswap-ui/typography.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import MuiTypography from '@material-ui/core/Typography';
 
-function Typography(props) {
-  return <MuiTypography {...props} />
-}
+const Typography = forwardRef((props, ref) => {
+  return <MuiTypography ref={ref} {...props} />
+});
 
 export default Typography;
