da9a72a92581e58e2f34cf777156ea9651f8e381
tphanson <tphanson@kambria.io>
2021-05-10 21:54:29 +0700

    export muicards

---
+++ src/senswap-ui/card.js
@@ -0,0 +1,35 @@
+import React, { forwardRef } from 'react';
+
+import MuiCard from '@material-ui/core/Card';
+import MuiCardActionArea from '@material-ui/core/CardActionArea';
+import MuiCardActions from '@material-ui/core/CardActions';
+import MuiCardContent from '@material-ui/core/CardContent';
+import MuiCardHeader from '@material-ui/core/CardHeader';
+import MuiCardMedia from '@material-ui/core/CardMedia';
+
+
+const Card = forwardRef((props, ref) => {
+  return <MuiCard {...props} ref={ref} />
+});
+
+export default Card;
+
+export const CardActionArea = forwardRef((props, ref) => {
+  return <MuiCardActionArea {...props} ref={ref} />
+});
+
+export const CardActions = forwardRef((props, ref) => {
+  return <MuiCardActions {...props} ref={ref} />
+});
+
+export const CardContent = forwardRef((props, ref) => {
+  return <MuiCardContent {...props} ref={ref} />
+});
+
+export const CardHeader = forwardRef((props, ref) => {
+  return <MuiCardHeader {...props} ref={ref} />
+});
+
+export const CardMedia = forwardRef((props, ref) => {
+  return <MuiCardMedia {...props} ref={ref} />
+});
