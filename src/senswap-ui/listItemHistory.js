03b48e4227316d5584a350dc5cef8f13ed65a3b3
thangDescartesNetwork <thangnguyen@descartes.network>
2021-05-11 15:29:02 +0700

    Adjust code style

--- src/senswap-ui/listItemHistory.js
+++ src/senswap-ui/listItemHistory.js
@@ -27,9 +27,39 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+// SVG icon for Deposit
+const DepositIcon = () => {
+  return (<SvgIcon style={{ fontSize: 40 }}
+    width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="20" cy="20" r="20" fill="#6C5DD3" />
+    <rect x="12.5" y="16.5" width="15" height="11" rx="1" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <circle cx="20" cy="22" r="2.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M14.5 14.5H25.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M16.5 12.5H23.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+  </SvgIcon>);
+}
+
+// SVG icon for Withdraw
+const WithdrawIcon = () => {
+  return (<SvgIcon style={{ fontSize: 40 }}
+    width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="20" cy="20" r="20" fill="#4FBF67" />
+    <path d="M22 16.5L24.5 12.5H15.5L18 16.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+    <path fillRule="evenodd" clipRule="evenodd" d="M22 16.5H18C18 16.5 13.5 19.722 13.5 23.056C13.5 25.278 15.125 27.5 20 27.5C24.875 27.5 26.5 25.278 26.5 23.056C26.5 19.722 22 16.5 22 16.5Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+  </SvgIcon>);
+}
+
 const ACTION = {
-  withdraw: 'Withdraw',
-  deposit: 'Deposit',
+  deposit: {
+    name: 'Deposit',
+    icon: DepositIcon,
+  },
+  withdraw: {
+    name: 'Withdraw',
+    icon: WithdrawIcon,
+  },
 }
 
 const dateFormat = { year: 'numeric', month: 'short', day: 'numeric' };
@@ -43,30 +73,13 @@ const ListItemHistory = props => {
   return <List className={defaultClasses.root} >
     <ListItem alignItems="flex-start" {...others} >
       <ListItemIcon>
-        {variant === 'deposit' ?
-          <SvgIcon style={{ fontSize: 40 }} // SVG icon for Deposit
-            width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="20" cy="20" r="20" fill="#6C5DD3" />
-            <rect x="12.5" y="16.5" width="15" height="11" rx="1" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-            <circle cx="20" cy="22" r="2.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M14.5 14.5H25.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M16.5 12.5H23.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-          </SvgIcon>
-          :
-          <SvgIcon style={{ fontSize: 40 }} // SVG icon for Withdraw
-            width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="20" cy="20" r="20" fill="#4FBF67" />
-            <path d="M22 16.5L24.5 12.5H15.5L18 16.5" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M22 16.5H18C18 16.5 13.5 19.722 13.5 23.056C13.5 25.278 15.125 27.5 20 27.5C24.875 27.5 26.5 25.278 26.5 23.056C26.5 19.722 22 16.5 22 16.5Z" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-          </SvgIcon>}
+        {ACTION[variant].icon()}
       </ListItemIcon>
       <ListItemText disableTypography
         primary={
           <Grid container spacing={2}>
             <Grid item xs={6}>
-              <Typography>{ACTION[variant]}</Typography>
+              <Typography>{ACTION[variant].name}</Typography>
             </Grid>
             <Grid item xs={6}>
               <Typography align="right">{new Date(date).toLocaleString("en-US", dateFormat)}</Typography>
